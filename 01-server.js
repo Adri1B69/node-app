8bc965d072da4b23e1a3e2ef3fd303f319090222
Adri1B69 <adrien.bonnefond@gmail.com>
2024-05-29 17:01:19 +0200

    Ajout Mongo

--- 01-server.js
+++ 01-server.js
@@ -1,31 +1,23 @@
 const express = require('express');
-const promClient = require('prom-client');
+const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 const message = process.env.WELCOME_MESSAGE || 'Bienvenue sur mon application!';
+const mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@mongodb-service:27017`;
 
-// Créer un compteur pour le nombre de requêtes sur la route "/"
-const requestCounter = new promClient.Counter({
-  name: 'http_requests_total',
-  help: 'Nombre total de requêtes HTTP reçues',
-  labelNames: ['method', 'route']
+let db;
+
+MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    db = client.db('test');
+    app.listen(port, () => {
+        console.log(`Application démarrée sur http://localhost:${port}`);
+    });
 });
-
-// Configurer Prometheus pour collecter les métriques par défaut
-promClient.collectDefaultMetrics();
 
 app.get('/', (req, res) => {
-  // Incrémenter le compteur pour chaque requête sur la route "/"
-  requestCounter.inc({ method: req.method, route: req.route.path });
-  res.send(message);
-});
-
-// Ajouter une route pour exposer les métriques Prometheus
-app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', promClient.register.contentType);
-  res.end(await promClient.register.metrics());
-});
-
-app.listen(port, () => {
-  console.log(`Application démarrée sur http://localhost:${port}`);
+    db.collection('messages').insertOne({ message }, (err, result) => {
+        if (err) throw err;
+        res.send(message);
+    });
 });
